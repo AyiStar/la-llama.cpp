14481f8c69509b5d96de5eca174b7305dd19731c
loongson <loongson@loongson-pc>
2024-04-20 22:04:58 +0800

    alter hf download

--- scripts/llama_weights_download.py
+++ scripts/llama_weights_download.py
@@ -3,16 +3,32 @@ from huggingface_hub import snapshot_download, hf_hub_download
 
 #TODO support more models with argparse
 
-# # original model weights
+# TOKEN = os.environ['HF_TOKEN']
+
+# original model weights
 # snapshot_download(
 #     repo_id="meta-llama/Llama-2-7b",
-#     token=TOKEN)
+#     local_dir="/home/loongson/llama_models/Llama-2-7B-GGUF",
+#     local_dir_use_symlinks=False,
+#     resume_download=True,
+#     token=TOKEN,
+# )
+
 
 # converted GGUF
-snapshot_download(
-    repo_id="TheBloke/Llama-2-70B-GGUF",
-    local_dir="/root/llama_models/Llama-2-70B-GGUF",
-    local_dir_use_symlinks=False,
-    resume_download=True,
-    max_workers=32,
-)
+# snapshot_download(
+#     repo_id="TheBloke/Llama-2-7B-GGUF",
+#     local_dir="/home/loongson/llama_models/Llama-2-7B-GGUF",
+#     local_dir_use_symlinks=False,
+#     resume_download=True,
+#     max_workers=1,
+# )
+
+for quant_type in ['Q4_0', 'Q4_K_S', 'Q4_K_M']:
+    hf_hub_download(
+        repo_id="TheBloke/Llama-2-7B-GGUF",
+        filename=f"llama-2-7b.{quant_type}.gguf",
+        local_dir="/home/loongson/llama_models/Llama-2-7B-GGUF",
+        local_dir_use_symlinks=False,
+        resume_download=True,
+    )
